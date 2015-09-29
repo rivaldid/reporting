f6f9661b8adc6be33cf51946acdaf51127fad737
vilardid <vilardid@posteitaliane.it>
2015-09-29 16:08:33 +0200

    fix population

--- population.sh
+++ population.sh
@@ -1,28 +1,61 @@
 #!/usr/bin/bash
 
 PREFIX="/home/vilardid/reporting"
+REPORT="/mnt/REPORT"
+
 LOG=$PREFIX"/population.log"
 MYARGS="-H -ureporting -preportuser -D reporting"
 
 if [ -f $LOG ]; then rm $LOG; fi
 touch $LOG
 
-INPUT="test.csv"
+sudo mount $REPORT
 
-iconv -f "windows-1252" -t "UTF-8" $INPUT -o temp.$INPUT
+#INPUT="test.csv"
 
-while read line; do
-	while IFS=';' read -ra field; do
-		centrale="${field[0]}"
-		ora="${field[1]}"
-		data="${field[2]}"
-		azione="${field[3]}"
-		messaggio="${field[4]}"
-	done <<< $line
+for file in $(find $REPORT -name "*.csv" -type f); do
+
+	INPUT=$file	
+	#TEMP=$PREFIX/"${INPUT##*/}".temp.csv
+	TEMP=$PREFIX/temp.csv
 	
-	mycall="CALL input_winwatch('$centrale','$ora','$data','$azione','$messaggio')"
-	#echo $mycall
-	mysql $MYARGS -e "$mycall \W;" >> $LOG
-done < temp.$INPUT
+	echo $INPUT >> $LOG
+	echo $TEMP >> $LOG
 
-rm temp.$INPUT
+	iconv -f "windows-1252" -t "UTF-8" $INPUT -o $TEMP
+
+	while read line; do
+		while IFS=';' read -ra field; do
+			
+			centrale="${field[0]}"
+			ora="${field[1]}"
+			data="${field[2]}"
+			azione="${field[3]}"
+			messaggio="${field[4]}"
+			
+			# extra whitespaces
+			centrale=($centrale)
+			ora=($ora)
+			data=($data)
+			azione=($azione)
+			messaggio=($messaggio)
+			
+			# cr
+			centrale=$(echo $centrale|tr -d '\n')
+			ora=$(echo $ora|tr -d '\n')
+			data=$(echo $data|tr -d '\n')
+			azione=$(echo $azione|tr -d '\n')
+			messaggio=$(echo $messaggio|tr -d '\n')
+			
+		done <<< $line
+		
+		mycall="CALL input_winwatch('${centrale[@]}','${ora[@]}','${data[@]}','${azione[@]}','${messaggio[@]}')"
+		echo $mycall >> $LOG
+		mysql $MYARGS -e "$mycall \W;" >> $LOG
+	done < $TEMP
+
+	rm $TEMP
+
+done
+
+sudo umount $REPORT
