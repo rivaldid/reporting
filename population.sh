e1b6160f8835b8b0e74c6977be80372c349a9804
vilardid <vilardid@posteitaliane.it>
2015-10-21 21:46:31 +0200

    ultima modifica... in culo alla balena

--- population.sh
+++ population.sh
@@ -3,11 +3,19 @@
 PREFIX="/home/vilardid/reporting"
 
 cd $PREFIX
+
 echo "==> winparse"
-./win_parse.sh
+time ./win_parse.sh
 if [ $? -eq 0 ]; then
 	echo "--> winparse done"
 else
 	echo "--> winparse fail"
 fi
 
+echo "==> serparse"
+time ./ser_parse.sh
+if [ $? -eq 0 ]; then
+	echo "--> serparse done"
+else
+	echo "--> serparse fail"
+fi
