1e026b8576bf8d469aef1f15c460d95e9d4d9980
vilardid <vilardid@posteitaliane.it>
2015-10-26 12:29:18 +0100

    backup restore dati

--- make.sh
+++ make.sh
@@ -2,10 +2,14 @@
 
 PREFIX="/home/vilardid/reporting"
 LOG=$PREFIX"/make.log"
+DUMPFILE=$PREFIX"/dumpfile.sql"
 MYARGS="-ureporting -preportuser -D reporting"
 
 if [ -f $LOG ]; then rm $LOG; fi
 touch $LOG
+
+echo "--> Dumping del db" >> $LOG
+mysqldump -ureporting -preportuser reporting > $DUMPFILE
 
 echo "--> Carico la base" >> $LOG
 mysql $MYARGS -e "source $PREFIX/base.sql \W;" >> $LOG
@@ -21,3 +25,7 @@ mysql $MYARGS -e "source $PREFIX/view.sql \W;" >> $LOG
 
 echo "--> Carico alcuni dati " >> $LOG
 mysql $MYARGS -e "source $PREFIX/dati.sql \W;" >> $LOG
+
+echo "--> Ripristino il dump" >> $LOG
+mysql $MYARGS -e "source $DUMPFILE \W;" >> $LOG
+rm $DUMPFILE
