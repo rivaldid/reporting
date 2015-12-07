e480aa295a6e0d350b1262e3ed61ce972a9fbd28
vilardid <vilardid@posteitaliane.it>
2015-12-07 16:40:23 +0100

    working on parser serchio

--- ser_parse.sh
+++ ser_parse.sh
@@ -20,13 +20,38 @@ rconcentratore='(.*)(\([0-9]{3}\))(.*)'
 rseriale='(.*)([0-9]{8})(.*)'
 rvarco='(.*)(H\([0-9]{2}\))(.*)'
 
+reventi_qutenzaq='(.*)([[:graph:]][[:space:]].*[[:space:]][[:graph:]])(.*)'
+reventi_nable='(.*)([[:alpha:]]{1,3}]BILITATO)(.*)'
+reventi_durata='(.*)([[:graph:]]durata.*[[:graph:]])(.*)'
+
 reventi='(.*)(' # 1-2
 reventi+='(Scasso[[:space:]]varco)|' #3
 reventi+='(Varco[[:space:]]chiuso)|' #4
 reventi+='(Varco[[:space:]]non[[:space:]]chiuso)|' #5
-reventi+='(Transito[[:space:]]effettuato)|' #6
-reventi+='(Tessera[[:space:]]inesistente)' #7
-reventi+=')(.*)' #8
+reventi+='(Varco[[:space:]]non[[:space:]]aperto)|' #6
+reventi+='(Transito[[:space:]]effettuato)|' #7
+reventi+='(Transito[[:space:]]non[[:space:]]consentito)|' #8
+reventi+='(Tessera[[:space:]]inesistente)|' #9
+reventi+='(Caduta[[:space:]]Linea[[:punct:]])|' #10
+reventi+='(Linea[[:space:]]Mini[[:space:]]Pulsar[[:punct:]])|' #11
+
+reventi+='(ADMIN[0-9][[:space:]]Tastiera[[:space:]]Abilitata[[:punct:]][[:space:]])|' #12
+reventi+='(VISUAL[[:space:]]Tastiera[[:space:]]Abilitata[[:punct:]][[:space:]])|' #13
+
+reventi+='(Allarmi[[:space:]]Acquisiti[[:graph:]].*[[:graph:]]{2}[[:space:]])|' #14
+reventi+='(Allarme[[:space:]]Tamper)|' #15
+
+reventi+='(Richiesta[[:space:]]Invio[[:space:]]Programmazione[[:space:]][[:punct:]][[:space:]])|' #16
+reventi+='(Fine[[:space:]]invio[[:space:]]dati[[:space:]]di[[:space:]]programmazione[[:punct:]])|' #17
+reventi+='(Comando[[:space:]]Cambio[[:space:]]Stato[[:space:]]Lettore)|' #18
+reventi+='(Richiesta[[:space:]]Comando[[:space:]]Apertura[[:space:]]Varco)|' #19
+reventi+='(Apertura[[:space:]]varco[[:space:]]Console[[:punct:]])|' #20
+reventi+='(Allarme[[:space:]]ingresso[[:space:]][0-9])|' #21
+reventi+='(Stato[[:space:]]Lettore)|' #22
+reventi+='(Fine[[:space:]]transito)|' #23
+reventi+='(Ripristino[[:space:]]Linea)' #24
+reventi+=')(.*)' #25
+reventi_max=25
 
 # /regex
 
@@ -89,17 +114,31 @@ for file in $(find $REPORT -name "*.xps" -type f); do
 					[[ ! "$target" =~ "- Stampa Report da" ]]; then
 
 					#echo "$target"
+					unset data centrale seriale evento varco direzione ospite eventi_nable eventi_durata eventi_utenza
 					printf -v buffer "$target"
 					
+					# trash
+					[[ $buffer =~ $rconcentratore ]] && buffer=${BASH_REMATCH[1]}${BASH_REMATCH[3]}
+					
+					# componenti
+					[[ $buffer =~ $reventi_nable ]] && eventi_nable=${BASH_REMATCH[2]} && buffer=${BASH_REMATCH[1]}${BASH_REMATCH[3]}
+					[[ $buffer =~ $reventi_durata ]] && eventi_durata=${BASH_REMATCH[2]} && buffer=${BASH_REMATCH[1]}${BASH_REMATCH[3]}
+					[[ $buffer =~ $reventi_qutenzaq ]] && eventi_utenza=${BASH_REMATCH[2]} && buffer=${BASH_REMATCH[1]}${BASH_REMATCH[3]}
+					
+					# contents
 					[[ $buffer =~ $rdata ]] && data=${BASH_REMATCH[2]} && buffer=${BASH_REMATCH[1]}${BASH_REMATCH[3]}
 					[[ $buffer =~ $rcentrale ]] && centrale=${BASH_REMATCH[2]} && buffer=${BASH_REMATCH[1]}${BASH_REMATCH[3]}
-					[[ $buffer =~ $rconcentratore ]] && buffer=${BASH_REMATCH[1]}${BASH_REMATCH[3]}
 					[[ $buffer =~ $rseriale ]] && seriale=${BASH_REMATCH[2]} && buffer=${BASH_REMATCH[1]}${BASH_REMATCH[3]}
 					[[ $buffer =~ $rvarco ]] && varco=${BASH_REMATCH[2]} && buffer=${BASH_REMATCH[1]}${BASH_REMATCH[3]}
+					[[ $buffer =~ $reventi ]] && evento=${BASH_REMATCH[2]} && buffer=${BASH_REMATCH[1]}${BASH_REMATCH[$reventi_max]}
 					
-					[[ $buffer =~ $reventi ]] && evento=${BASH_REMATCH[2]} && buffer=${BASH_REMATCH[1]}${BASH_REMATCH[8]}
-										
+					# concateno i componenti
+					[[ -n $eventi_nable ]] && printf -v evento "%s %s" "$evento" "$eventi_nable"
+					[[ -n $eventi_durata ]] && printf -v evento "%s %s" "$evento" "$eventi_durata"
+					[[ -n $eventi_utenza ]] && printf -v ospite "%s %s" "$ospite" $(echo "$eventi_utenza" | tr -d '[|]')
+									
 					mycall="CALL input_serchio('$data','$centrale','$seriale','$evento','$varco','$direzione','$ospite','$checksum');"
+					#printf -v mycall "CALL input_serchio('%s','%s','%s','%s','%s','%s','%s','%s');" $(unspace "$data") $(unspace "$centrale") $(unspace "$seriale") $(unspace "$evento") $(unspace "$varco") $(unspace "$direzione") $(unspace "$ospite") "$checksum"
 					#mycall="CALL input_serchio($(perl ser_parse_core.pl "$target"),'$checksum');"
 					
 					echo "$mycall" >> $LOG
