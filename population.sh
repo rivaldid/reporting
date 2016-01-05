2235dff89df47a570a8b44cc6d7085b4571f2485
vilardid <vilardid@posteitaliane.it>
2016-01-05 15:36:25 +0100

    adc in population

--- population.sh
+++ population.sh
@@ -9,7 +9,7 @@ touch $LOG
 
 echo "*** BEGIN " $(date) "***" >> $LOG
 
-echo "==> winparse" >> $LOG
+echo "==> win_parse" >> $LOG
 { time ./win_parse.sh >> $LOG; } 2>> $LOG
 if [ $? -eq 0 ]; then
 	echo "--> winparse terminato con successo" >> $LOG
@@ -17,12 +17,20 @@ else
 	echo "--> winparse terminato senza lavoro" >> $LOG
 fi
 
-echo "==> serparse" >> $LOG
+echo "==> ser_parse" >> $LOG
 { time ./ser_parse.sh >> $LOG; } 2>> $LOG
 if [ $? -eq 0 ]; then
 	echo "--> serparse terminato con successo" >> $LOG
 else
 	echo "--> serparse terminato senza lavoro" >> $LOG
+fi
+
+echo "==> adc_parse" >> $LOG
+{ time ./adc_parse.sh >> $LOG; } 2>> $LOG
+if [ $? -eq 0 ]; then
+	echo "--> adcparse terminato con successo" >> $LOG
+else
+	echo "--> adcparse terminato senza lavoro" >> $LOG
 fi
 
 echo "*** END " $(date) "***" >> $LOG
