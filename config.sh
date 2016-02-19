8a9615d721f59fd88ed6b6905a406f82a5ee81c6
vilardid <vilardid@posteitaliane.it>
2016-02-19 10:26:22 +0100

    identificazione del sistem operativo

--- config.sh
+++ config.sh
@@ -3,15 +3,34 @@
 # basic configs
 
 # passwords
-FILE_PASSWORD="/home/vilardid/account_db.txt"
-source "$FILE_PASSWORD"
+#FILE_PASSWORD="/home/vilardid/account_db.txt"
+#source "$FILE_PASSWORD"
 
 # mysql arguments user
-MYARGS="-ureporting -p$pass_reporting"
+#MYARGS="-ureporting -p$pass_reporting"
 #MYARGS="--login-path=reporting"
 # mysql arguments root
-MYARGS1="-uroot -p$pass_root"
+#MYARGS1="-uroot -p$pass_root"
 #MYARGS1="--login-path=root"
+
+system="$(cat /etc/system-release)"
+redhat='Red[[:space:]]Hat[[:space:]]Enterprise[[:space:]]Linux[[:space:]]Server'
+centos='CentOS[[:space:]]Linux'
+
+if [[ $system =~ $redhat ]]; then 
+	echo "Trovato sistema operativo Red Hat"
+	MYARGS="--login-path=reporting"
+	MYARGS1="--login-path=root"
+elif [[ $system =~ $centos ]]; then 
+	echo "Trovato sistema operativo CentOS"
+	FILE_PASSWORD="/home/vilardid/account_db.txt"
+	source "$FILE_PASSWORD"
+	MYARGS="-ureporting -p$pass_reporting"
+	MYARGS1="-uroot -p$pass_root"
+else
+	echo "Sistema operativo non trovato o non compatibile"
+	exit 1
+fi
 
 # history logfile
 WIN_HISTORY="$PREFIX/win_parse.history.log"
