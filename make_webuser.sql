e7402f6339fd1a45bd105aaad389a9f8b989607a
vilardid <vilardid@posteitaliane.it>
2016-02-19 09:49:09 +0100

    webuser

--- make_webuser.sql
+++ make_webuser.sql
@@ -1,4 +1,4 @@
-CALL administration.drop_user('webreporting',@res); 
+CALL administration.drop_user('webreporting',@res);
 SELECT @res;
 CREATE USER 'webreporting'@'%' IDENTIFIED BY 'w3bR3port.';
 GRANT EXECUTE ON FUNCTION reporting.html_unencode TO 'webreporting'@'%';
@@ -7,6 +7,6 @@ GRANT SELECT ON reporting.SERCHIO_OSPITI TO 'webreporting'@'%';
 GRANT SELECT ON reporting.BADGES TO 'webreporting'@'%';
 GRANT SELECT ON reporting.WINWATCH TO 'webreporting'@'%';
 GRANT SELECT ON reporting.ADC TO 'webreporting'@'%';
-GRANT SELECT ON magazzino.vserv_trace TO 'webreporting'@'%';
+#GRANT SELECT ON magazzino.vserv_trace TO 'webreporting'@'%';
 GRANT EXECUTE ON PROCEDURE reporting.PASSAGGI TO 'webreporting'@'%';
 FLUSH PRIVILEGES;
