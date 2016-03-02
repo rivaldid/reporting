477ecd0bc49b68d2212efd77ce4468249d4a675d
vilardid <vilardid@posteitaliane.it>
2016-03-02 14:10:47 +0100

    aggiornamento pass su host remoto

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+FILE_PASS="/home/vilardid/account_ad.txt"
+
+echo "==> Sync password to remote server"
+scp "$FILE_PASS" vilardid@dcserver2:"$FILE_PASS"
+echo "==> Sync password to remote server done!"
+
 echo "==> Sync server"
 . /home/vilardid/reporting/sync_report.sh
 echo "==> Sync server done!"
