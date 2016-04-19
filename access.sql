6a55445a8bbb2f14dac33f1d780fbcf5198c8838
vilardid <vilardid@posteitaliane.it>
2016-04-19 12:49:14 +0200

    fix access

--- access.sql
+++ access.sql
@@ -3,5 +3,5 @@ USE `reporting`;
 DROP VIEW IF EXISTS `search_serchio_ospiti`;
 
 CREATE VIEW `search_serchio_ospiti` AS
-SELECT DATE(Data) DateOnly, Ospite FROM SERCHIO WHERE
-GROUP BY DateOnly, Ospite;
+SELECT DATE(Data) DateOnly, Ospite FROM SERCHIO
+GROUP BY DateOnly,Ospite ORDER BY DateOnly DESC;
