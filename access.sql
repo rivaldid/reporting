60968254dc1f17c033ebde6ee6e45e284aea1aa1
vilardid <vilardid@posteitaliane.it>
2016-04-26 17:30:11 +0200

    non funziona ancora ma ok lo stesso

--- access.sql
+++ access.sql
@@ -1,79 +1,57 @@
 USE `reporting`;
 
-DROP VIEW IF EXISTS `search_serchio_ospiti`;
+DROP PROCEDURE IF EXISTS `ACCESSI_SERCHIO`;
 
-CREATE VIEW `search_serchio_ospiti` AS
-SELECT DATE(Data) DateOnly, Ospite FROM SERCHIO
-GROUP BY DateOnly,Ospite ORDER BY DateOnly DESC;
-
-/*
 DELIMITER $$
 
-CREATE PROCEDURE `minmax_serchio`(IN in_data DATETIME, IN in_ospite VARCHAR(45))
+CREATE PROCEDURE `ACCESSI_SERCHIO`(IN in_data DATE, IN in_ospite VARCHAR(45))
 BEGIN
 
-DECLARE temp_data DATETIME;
-DECLARE temp_centrale VARCHAR(45);
-DECLARE temp_messaggio VARCHAR(45);
+DECLARE temp_data DATE;
+DECLARE temp_ospite VARCHAR(45);
+DECLARE temp_in TIME;
+DECLARE temp_out TIME;
 
-DECLARE min_data DATETIME;
-DECLARE min_centrale VARCHAR(45);
-DECLARE min_messaggio VARCHAR(45);
+declare max_data DATE;
 
-DECLARE max_data DATETIME;
-DECLARE max_centrale VARCHAR(45);
-DECLARE max_messaggio VARCHAR(45);
 
-DECLARE done INT DEFAULT FALSE;
+DROP TEMPORARY TABLE IF EXISTS accessi;
+CREATE TEMPORARY TABLE accessi(
+data date,
+ospite varchar(45),
+ingresso time,
+uscita time);
 
-DECLARE query CURSOR FOR 
-	SELECT Data,Centrale,Messaggio 
+SELECT MAX(DATE(Data)) INTO max_data FROM SERCHIO;
+
+REPEAT
+
+	SELECT 
+	DATE(Data) DateOnly, 
+	Ospite, 
+	MIN(DATE_FORMAT(Data,'%H:%i:%s')) INGRESSO, 
+	MAX(DATE_FORMAT(Data,'%H:%i:%s')) USCITA 
+	INTO
+	temp_data,
+	temp_ospite,
+	temp_in,
+	temp_out
 	FROM SERCHIO 
 	WHERE 
-		DATE(Data) = in_data AND 
-		Ospite LIKE CONCAT(in_ospite,'%');
+	DATE(Data) = in_data AND
+	Ospite <> '' AND 
+	Ospite LIKE CONCAT(in_ospite,'%')
+	GROUP BY DateOnly, Ospite;
 
-DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-
-OPEN query;
-
-FETCH query INTO min_data,min_centrale,min_messaggio;
-FETCH query INTO max_data,max_centrale,max_messaggio;
-
-read_loop: LOOP
-
-	FETCH query INTO temp_data,temp_centrale,temp_messaggio;
+	INSERT INTO accessi(data,ospite,ingresso,uscita) VALUES(temp_data,temp_ospite,temp_in,temp_out);
 	
-	IF done THEN
-		LEAVE read_loop;
-	END IF;
-	
-	IF (temp_data < min_data) THEN
-		SET min_data = temp_data;
-		SET min_centrale = temp_centrale;
-		SET min_messaggio = temp_messaggio;
-	END IF;
+	SET in_data = (SELECT in_data + INTERVAL 1 DAY);
 
-	IF (temp_data > max_data) THEN
-		SET max_data = temp_data;
-		SET max_centrale = temp_centrale;
-		SET max_messaggio = temp_messaggio;
-	END IF;
-	
+UNTIL (in_data <= max_data) END REPEAT;
 
-END LOOP read_loop;
-
-CLOSE query;
-
-SELECT 
-	in_data,
-	in_ospite,
-	CONCAT_WS(' ','PULSAR',min_centrale,min_messaggio,DATE_FORMAT(min_data,'%H:%i:%s')) INGRESSO,
-	CONCAT_WS(' ','PULSAR',max_centrale,max_messaggio,DATE_FORMAT(max_data,'%H:%i:%s')) USCITA,
-	TIMESTAMPDIFF(MINUTE,max_data,min_data) DURATA;
+SELECT * FROM accessi;
 
 END;
 $$
 
 DELIMITER ;
-*/
